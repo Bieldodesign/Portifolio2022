dd082d402cd086344cda58f50b7235e523aaf81e
Gabriel <bieldodesign@gmail.com>
2022-05-28 13:47:47 -0300

    28 05
    falta só ver se add mais no miolo + sklls tree

--- app.js
+++ app.js
@@ -7,3 +7,21 @@ modalBtns.forEach(function (btn) {
         document.getElementById(modal).style.display = "block";
     };
 });
+var closeBtns = document.querySelectorAll(".closebtn");
+
+closeBtns.forEach(function(btn){
+        btn.onclick = function(){
+    var modal = btn.closest(".modal").style.display = "none";
+        };
+});
+
+
+window.onclick = function(e){
+    if(e.target.className === "modal"){
+        e.target.style.display = "none";
+    }
+};
+
+
+
+
